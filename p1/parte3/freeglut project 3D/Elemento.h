293e4c4e299be854142e6a9bb4003c537c7d2999
Hristo Ivanov <hristo.idgh@gmail.com>
2016-03-15 16:58:52 +0100

    P1. Parte3. Pequeño Avance.

---
+++ p1/parte3/freeglut project 3D/Elemento.h
@@ -0,0 +1,32 @@
+#ifndef Elemento_H_
+#define Elemento_H_
+
+#include "PuntoVector3D.h"
+#include "Cara.h"
+#include "Color.h"
+
+class Elemento {
+protected:
+	int numeroVertices;
+	PuntoVector3D** vertice;
+	int numeroNormales;
+	PuntoVector3D** normal;
+	int numeroCaras;
+	Cara** cara;
+	int numeroColores;
+	Color** color;
+
+	PuntoVector3D posicion;
+	PuntoVector3D rotacion;
+	PuntoVector3D escalado;
+
+public:
+	Elemento();
+	~Elemento();
+	int getNumeroVertices();
+	int getNumeroNormales();
+	int getNumeroCaras();
+	virtual void dibuja();
+	PuntoVector3D* CalculoVectorNormalPorNewell(Cara* c);
+};
+#endif
