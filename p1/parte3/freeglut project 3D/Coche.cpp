f450c26361e7a3a2d7fb93b3aa23f49cf308c408
Hristo Ivanov <hristo.idgh@gmail.com>
2016-03-31 17:47:43 +0200

    P1. Parte2 3. Revisada.

--- p1/parte3/freeglut project 3D/Coche.cpp
+++ p1/parte3/freeglut project 3D/Coche.cpp
@@ -25,13 +25,13 @@ Coche::Coche() {
 	elemento[4]->setRotacion(0.0, 180.0, 0.0);
 	elemento[4]->setPosicion(1.5, -1.5, -1.5);
 
-	elemento[5] = new Cilindro(1.7, 4, 4);
+	elemento[5] = new Cilindro(1.7, 10, 10);
 	elemento[5]->setEscalado(.2, .2, 1.0);
 	elemento[5]->setRotacion(0.0, 90, 0.0);
 	elemento[5]->setPosicion(1.5, 0.0, -0.5);
 	elemento[5]->setColor(0, 1.0, .0);
 
-	elemento[6] = new Cilindro(1.7, 4, 4);
+	elemento[6] = new Cilindro(1.7, 10, 10);
 	elemento[6]->setEscalado(.2, .2, 1.0);
 	elemento[6]->setRotacion(0.0, 90, 0.0);
 	elemento[6]->setPosicion(1.5, 0.0, 0.5);
@@ -42,10 +42,9 @@ Coche::Coche() {
 void Coche::avanza(GLfloat f){
 	this->posicion[0] = this->posicion[0] + f;
 
-
-	elemento[1]->rotacion[2] = elemento[1]->rotacion[2] - 2 * 3.14 * f;
-	elemento[2]->rotacion[2] = elemento[2]->rotacion[2] - 2 * 3.14 * f;
-	elemento[3]->rotacion[2] = elemento[3]->rotacion[2] + 2 * 3.14 * f;
-	elemento[4]->rotacion[2] = elemento[4]->rotacion[2] + 2 * 3.14 * f;
+	elemento[1]->rotacion[2] = elemento[1]->rotacion[2] - (f / 3.14) * 180.0f;
+	elemento[2]->rotacion[2] = elemento[2]->rotacion[2] - (f / 3.14) * 180.0f;
+	elemento[3]->rotacion[2] = elemento[3]->rotacion[2] + (f / 3.14) * 180.0f;
+	elemento[4]->rotacion[2] = elemento[4]->rotacion[2] + (f / 3.14) * 180.0f;
 
 }
